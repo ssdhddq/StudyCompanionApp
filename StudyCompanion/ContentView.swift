b694ea6a46a54d59979325b126ecb9fa9a78f877
ssdhddq <ssdhddq@vk.com>
2025-03-27 18:11:42 +0700

    Initial commit

---
+++ StudyCompanion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyCompanion
+//
+//  Created by SSDHDDQ on 27.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyCompanion/StudyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCompanionApp.swift
+//  StudyCompanion
+//
+//  Created by SSDHDDQ on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct StudyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
